ec716f320a028c72162c33be627cb2b995794de9
Farmermachine <85457833+Farmermachine@users.noreply.github.com>
2021-06-08 09:18:42 +0200

    Add files via upload

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -105,7 +105,7 @@ export default function Menu() {
 
       {open && (
         <MenuFlyout>
-          <MenuItem id="link" href="https://kishu.finance/">
+          <MenuItem id="link" href="https://escobar.financial">
             <Info size={14} />
             About
           </MenuItem>
@@ -113,7 +113,7 @@ export default function Menu() {
             <Code size={14} />
             Code
           </MenuItem>
-          <MenuItem id="link" href="https://t.me/kishuinucommunity">
+          <MenuItem id="link" href="https://t.me/eSCOBAR">
             <MessageCircle size={14} />
             Telegram
           </MenuItem>
